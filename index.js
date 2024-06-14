3174cef12d9a0069f11a4c3289046049b098d78b
tonmoy <tonmoy3673@gmail.com>
2024-06-14 17:43:39 +0600

    :bulb: installed stripe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 const port = 5000;
 const cors=require('cors')
 require("dotenv").config();
