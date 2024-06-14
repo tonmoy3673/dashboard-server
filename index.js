3340d9aa5c5e424d23c7edddee73be5badb50ad7
tonmoy <tonmoy3673@gmail.com>
2024-06-14 18:23:44 +0600

    :tada: implemented stripe payment method

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
 
     const booksData=client.db('booksData')
     const booksCollection=booksData.collection('booksCollection')
+    const paymentsCollection=booksData.collection('paymentsCollection');
     
     // ========= Add Products =========//
     app.post('/books',async(req,res)=>{
@@ -63,6 +64,41 @@ async function run() {
       )
       res.send(result)
     })
+
+    // ============ payment section =======//
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+          currency: 'usd',
+          amount: amount,
+          "payment_method_types": [
+              "card"
+          ]
+      });
+      res.send({
+          clientSecret: paymentIntent.client_secret,
+      });
+
+  });
+
+  app.post('/payments', async (req, res) => {
+    const payment = req.body;
+    const result = await paymentsCollection.insertOne(payment);
+    const id = payment.bookingId
+    const filter = { _id: ObjectId(id) }
+    const updatedDoc = {
+        $set: {
+            paid: true,
+            transactionId: payment.transactionId
+        }
+    }
+    const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+    res.send(result);
+})
+
    
     console.log("Connected to MongoDB!");
   } finally {
